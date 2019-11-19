4fa7b1e663ffac862891fc272e3d2145dd236379
shono09835 <51314469+shono09835@users.noreply.github.com>
2019-11-19 12:20:32 +0900

    Update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -12,7 +12,24 @@
         <p>Env Test: <?php print (array_key_exists('SOME_VAR', $_ENV) ? $_ENV['SOME_VAR'] : 'None') ?></p>
         <p>Version: <?php print phpversion(); ?></p>
         <?php
-             sleep ( rand ( 0, 10 ));
+            $number = mt_rand(1, 980); //980までの数値からrandomで整数値を1つ選ぶ
+            switch (true){
+            case $number <= 728: //1～728(10ms-100ms)
+                $x = mt_rand(10, 100);
+                usleep($x * 1000);
+                break;
+            case $number <= 899: //729～899(100ms-500ms)
+                $x = mt_rand(100, 500);
+                usleep($x * 1000);
+                break;
+            case $number <= 951: //900～951(0ms-10ms)
+                $x = mt_rand(0, 10);
+                usleep($x * 1000);
+                break;
+            default: //952～980(500ms-1000ms)
+                $x = mt_rand(500, 1000);
+                usleep($x * 1000);
+            }
         ?>
     </body>
-</html>
+</html>:wq
